8d9a276b50c77583b5b386499c1679d0aaee5248
D.M.Anuradha Kumara Dissanayake <d.m.a.k.dissanayake.1997@gmail.com>
2022-12-16 22:13:22 +0530

    Update some files

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SingInScreen extends StatefulWidget {
+  const SingInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SingInScreen> createState() => _SingInScreenState();
+}
+
+class _SingInScreenState extends State<SingInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors:  )
+        ),
+      ),
+    );
+  }
+}
