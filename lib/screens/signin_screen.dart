f7f9133d748937932e1fb482368a44c1a92e6881
D.M.Anuradha Kumara Dissanayake <d.m.a.k.dissanayake.1997@gmail.com>
2022-12-17 00:48:27 +0530

    Update 2 pages

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/material.dart';
+import  'package:flutter/material.dart';
+import 'package:login_page/screens/signup_screen.dart';
+import 'package:login_page/utils/color_utils.dart';
+
+import '../reuseble_widgets/reuseable_widget.dart';
 
 class SingInScreen extends StatefulWidget {
   const SingInScreen({Key? key}) : super(key: key);
@@ -8,15 +12,82 @@ class SingInScreen extends StatefulWidget {
 }
 
 class _SingInScreenState extends State<SingInScreen> {
+  final TextEditingController _passwordTexrController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              colors:  )
+            colors: [
+              hexStringToColor("0000FF"),
+              hexStringToColor("9546C4"),
+              hexStringToColor("FF1493"),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget('assets/img/as1.png'),
+                const SizedBox(
+                  height: 30,
+                ),
+                resuableTextField('Enter Username', Icons.person_outline, false,
+                    _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                resuableTextField('Enter Password', Icons.lock_outline, false,
+                    _passwordTexrController),
+                const SizedBox(
+                  height: 20,
+                ),
+                button(context, true, () {}),
+                signUpOption(),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don't have account ?",
+          style: TextStyle(
+            color: Colors.white70,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignUpScreen()),
+            );
+          },
+          child: const Text(
+              'Sign Up',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
